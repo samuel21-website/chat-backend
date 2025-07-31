145416f72532add4a0109faced197dff160d01cd
samuel21 <topgunmaverick0121@gmail.com>
2025-07-31 21:15:57 +0900

    � CORS 설정: Netlify 도메인 허용

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const http = require('http');
-const cors = require('cors');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors());
+
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://fancy-cascaron-97aca7.netlify.app',
     methods: ['GET', 'POST'],
   },
 });
-
-app.use(cors());
-app.use(express.json());
 
 io.on('connection', (socket) => {
   console.log('✅ 연결됨:', socket.id);
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('sendMessage', ({ roomId, message }) => {
-    console.log('💌 메시지 수신:', message);
+    console.log('💬 메시지 수신:', message);
     io.to(roomId).emit('receiveMessage', message);
   });
 
